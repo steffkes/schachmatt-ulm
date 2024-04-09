6beea664cdc9522f19d189518f5a5bc2c9234056
steffkes <stefan@gelungen.es>
2024-04-09 15:04:01 +0200

    first draft for our website

--- app.vue
+++ app.vue
@@ -1,5 +1,146 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <section
+    class="section"
+    style="
+      min-height: 50vh;
+      background-image: url(/images/P10003192670.jpg);
+      background-size: cover;
+      background-position: 50% 50%;
+    "
+  >
+    <div class="container is-max-desktop">
+      <div class="columns">
+        <div class="column is-two-fifths">
+          <img src="/schachmatt-ulm.svg" />
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section">
+    <div class="container is-max-desktop">
+      <p>
+        Schon wieder ein Feuerwehr-Treppenlauf? Ja doch! Aber natürlich anders =) Nachdem
+        ihr euch in der ersten Runde qualifiziert habt, geht es im K.O.-System
+        weiter! Bis am Ende das schnellste Team als Sieger feststeht.
+      </p>
+
+      <section class="level mt-6">
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="title">161</p>
+            <p class="footing">Teams</p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="title">16</p>
+            <p class="footing">Stockwerke</p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="title">8</p>
+            <p class="footing">Durchgänge</p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="title">1</p>
+            <p class="footing">Sieger</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  </section>
+
+  <section
+    class="section"
+    style="
+      min-height: 50vh;
+      background-image: url(/images/P10003192560.jpg);
+      background-size: cover;
+      background-position: 50% 50%;
+      background-attachment: fixed;
+    "
+  ></section>
+
+  <section class="section">
+    <div class="container is-max-desktop">
+      <p>
+        Flo war eindeutig froh, als er zum letzten Mal oben angekommen ist ;)
+      </p>
+    </div>
+  </section>
+
+  <section
+    class="section mt-6"
+    style="
+      min-height: 50vh;
+      background-image: url(/images/P10003192586.jpg);
+      background-size: cover;
+      background-position: 50% 50%;
+      background-attachment: fixed;
+    "
+  ></section>
+
+  <section class="section">
+    <div class="container is-max-desktop">
+      <p>Das ist euer Ausblick nach dem Zieleinlauf.</p>
+    </div>
+  </section>
+
+  <section
+    class="section mt-6"
+    style="
+      min-height: 50vh;
+      background-image: url(/images/P10003192613.jpg);
+      background-size: cover;
+      background-position: 50% 50%;
+      background-attachment: fixed;
+    "
+  ></section>
+
+  <section class="section">
+    <div class="container is-max-desktop">
+      <p>Denn unsere Wettkampf-Location ist das Maritim Hotel Ulm!</p>
+    </div>
+  </section>
+
+  <section
+    class="section mt-6"
+    style="
+      min-height: 75vh;
+      background-image: url(/images/P10003192583.jpg);
+      background-size: cover;
+      background-position: 50% 50%;
+      background-attachment: fixed;
+    "
+  ></section>
+
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Schachmatt Ulm</strong> wird organisiert von Stefan, dem
+        Initiator des Schanzenlauf Oberstdorf.
+      </p>
+    </div>
+  </footer>
 </template>
+
+<style>
+@use "bulma";
+</style>
+
+<script setup>
+useSeoMeta({
+  title: "Schachmatt Ulm",
+  script: [
+    {
+      src: "https://plausible.io/js/script.js",
+      "data-domain": "schachmatt-ulm.de",
+      defer: true,
+    },
+  ],
+});
+</script>
