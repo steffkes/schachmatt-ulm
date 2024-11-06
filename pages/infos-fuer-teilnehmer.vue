bdea64f4f57a40766ffda6f16b3b5ab43fb001d0
steffkes <stefan@gelungen.es>
2024-11-06 23:59:29 +0100

    maritim's accomodation offer

--- pages/infos-fuer-teilnehmer.vue
+++ pages/infos-fuer-teilnehmer.vue
@@ -83,6 +83,76 @@
       </p>
     </div>
   </section>
+
+  <div
+    class="mt-6"
+    style="
+      min-height: 50vh;
+      background-image: url(/images/P10003192527.jpg);
+      background-size: cover;
+      background-position: 50% 50%;
+      background-attachment: fixed;
+    "
+  ></div>
+
+  <section class="section">
+    <div class="container is-max-desktop">
+      <h2 class="title mt-6">
+        Übernachtungsangebot des Maritim Hotel Ulm
+        <p class="subtitle">Freitag, 07. — Sonntag, 09. Februar 2025</p>
+      </h2>
+
+      <div class="content">
+        <blockquote>
+          <table>
+            <thead>
+              <tr>
+                <th>Kategorie</th>
+                <th>Superior</th>
+                <th>Comfort</th>
+                <th>Classic</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>Zimmerpreis pro Tag</th>
+
+                <td>EZ: 139.90 €<br />DZ: 175.80 €</td>
+
+                <td>EZ: 119.90 €<br />DZ: 155.80 €</td>
+
+                <td>EZ: 109.90 €<br />DZ: 145.80 €</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <p>
+            Das Zimmerkontingent ist
+            <strong>bis zum 17. Januar 2025</strong> von den Teilnehmern selbst
+            unter dem Stichwort - <strong>Schachmatt</strong> - abzurufen.
+            Anschließend gehen die nicht abgerufenen Zimmereinheiten in den
+            freien Verkauf zurück. Die Hotelzimmer können nur per Email unter
+            dem vereinbarten Stichwort abgerufen werden:
+            <a href="mailto:reservierung.ulm@maritim.de"
+              >reservierung.ulm@maritim.de</a
+            >
+          </p>
+
+          <p>In unseren Zimmerpreisen sind folgende Leistungen enthalten:</p>
+          <ul>
+            <li>Übernachtung in einem Zimmer der gebuchten Kategorie</li>
+            <li>Teilnahme am Maritim Business-Frühstück</li>
+            <li>freie Benutzung des Schwimmbades</li>
+          </ul>
+        </blockquote>
+
+        <p>
+          Das Angebot gilt für Übernachtungen von Freitag, 07. auf Samstag, 08.
+          und/oder Samstag, 08. auf Sonntag, 09. Februar 2025.
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
