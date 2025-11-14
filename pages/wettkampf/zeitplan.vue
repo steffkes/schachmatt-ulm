776d5f1c0c4730fb3dc51ca08db9a8d96fd48b16
steffkes <stefan@gelungen.es>
2025-11-14 22:30:22 +0100

    add provisional time table

---
+++ pages/wettkampf/zeitplan.vue
@@ -0,0 +1,83 @@
+<script setup>
+useSeoMeta({
+  title: "Zeitplan / Wettkampf",
+  description: "Was, wann & wo stattfindet",
+});
+</script>
+
+<template>
+  <section class="section">
+    <div class="container is-max-desktop">
+      <h1 class="title">Der zeitliche Ablauf</h1>
+
+      <div class="content">
+        <h3>Samstag</h3>
+
+        <ul>
+          <li><span class="tag is-light">14:00-16:00</span> Überraschung</li>
+          <li>
+            <span class="tag is-light">17:00-19:00</span> Ausgabe der
+            Startnummern
+          </li>
+          <li>
+            <span class="tag is-light">18:00-20:00</span> Pasta-Party @ Maritim
+            Hotel Ulm
+          </li>
+        </ul>
+
+        <p>(Genaueres wird jeweils noch bekannt gegeben)</p>
+
+        <h3>Sonntag</h3>
+
+        <ul>
+          <li>
+            <span class="tag is-light">09:30-10:30</span> Ausgabe der
+            Startunterlagen (im Eingangsbereich des Maritim Hotel Ulm)
+          </li>
+          <li>
+            <span class="tag is-light">11:00</span> Startschuss für die
+            Qualifikationsrunde
+          </li>
+        </ul>
+
+        <!--
+              <dt>
+                <time class="has-text-grey-light">08:30 Uhr</time>
+              </dt>
+              <dd>Start Runde Q: alle Teams (Qualifikation)</dd>
+              <dd>Ein Team alle 30 Sekunden</dd>
+
+              <dt><time class="has-text-grey-light">10:00 Uhr</time></dt>
+              <dd>Start Runde 1: 128 Teams</dd>
+              <dd>Eine Paarung alle 30 Sekunden</dd>
+
+              <dt><time class="has-text-grey-light">11:00 Uhr</time></dt>
+              <dd>Start Runde 2: 64 Teams</dd>
+              <dd>Eine Paarung alle 30 Sekunden</dd>
+
+              <dt><time class="has-text-grey-light">11:30 Uhr</time></dt>
+              <dd>Start Runde 3: 32 Teams</dd>
+              <dd>Eine Paarung jede Minute</dd>
+
+              <dt><time class="has-text-grey-light">12:00 Uhr</time></dt>
+              <dd>Start Runde 4: 16 Teams (Achtelfinale)</dd>
+              <dd>Eine Paarung jede Minute</dd>
+
+              <dt><time class="has-text-grey-light">12:30 Uhr</time></dt>
+              <dd>Start Runde 5: 8 Teams (Viertelfinale)</dd>
+              <dd>Eine Paarung alle zwei Minuten</dd>
+
+              <dt><time class="has-text-grey-light">13:00 Uhr</time></dt>
+              <dd>Start Runde 6: 4 Teams (Halbfinale)</dd>
+              <dd>Eine Paarung alle zwei Minuten</dd>
+
+              <dt><time class="has-text-grey-light">13:30 Uhr</time></dt>
+              <dd>Start Runde 7: 2 Teams (Finale, groß & klein)</dd>
+
+              <dt><time class="has-text-grey-light">13:45 Uhr</time></dt>
+              <dd>Siegerehrung</dd>
+              -->
+      </div>
+    </div>
+  </section>
+</template>
