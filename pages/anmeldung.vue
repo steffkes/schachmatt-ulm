09b58fc06c6133d2f9bb397675b4d36fddb6f2b6
steffkes <stefan@gelungen.es>
2025-11-16 07:36:57 +0100

    highlight that registration is not yet open

--- pages/anmeldung.vue
+++ pages/anmeldung.vue
@@ -3,9 +3,12 @@
 <template>
   <section class="section">
     <div class="container is-max-desktop">
+      <div class="notification is-warning">
+        <p>Anmeldung ist noch nicht eröffnet</p>
+      </div>
       <p class="mb-4">
-        Anmeldung ist noch nicht eröffnet - alle Teilnehmer der ersten Ausgabe
-        werden automatisch benachrichtigt sobald es los geht.
+        Alle Teilnehmer der ersten Ausgabe werden automatisch benachrichtigt
+        sobald es los geht.
         <a href="https://airtable.com/appZnWtcgYgbjEPBs/shrqwZ67Y0WiuJfxA"
           >Sag mir auch bescheid, wenn's so weit ist!</a
         >
