015896353865596f7764b962550dbc68555afef5
steffkes <stefan@gelungen.es>
2025-11-18 19:35:56 +0100

    link to interest from registration page

--- pages/anmeldung.vue
+++ pages/anmeldung.vue
@@ -3,6 +3,17 @@
 <template>
   <section class="section">
     <div class="container is-max-desktop">
+      <div class="notification is-info">
+        <p>
+          Aktuell dürfe sich nur Teilnehmer der 1. Edition mit ihrem Code
+          anmelden.
+
+          <a href="https://airtable.com/appZnWtcgYgbjEPBs/shrqwZ67Y0WiuJfxA"
+            >Ich möchte auch! Sag mir bescheid, wenn's so weit ist!</a
+          >
+        </p>
+      </div>
+
       <p class="mb-4">
         Teilnahme als Team kostet 70 €. Bitte
         <NuxtLink href="/wettkampf/regeln"
