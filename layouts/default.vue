b9c42f215b02d9e941e74c091d95c92ac790bbd6
steffkes <stefan@gelungen.es>
2026-02-02 11:56:21 +0100

    refactor footer into separate layer
    TODO: how to manage assets within a layer?

--- layouts/default.vue
+++ layouts/default.vue
@@ -107,113 +107,12 @@
     "
   ></div>
 
-  <footer class="footer">
-    <div class="container is-max-desktop">
-      <div class="columns">
-        <div class="column">
-          <div class="columns">
-            <div class="column is-2 is-6-mobile">
-              <img src="/images/stefan.jpg" alt="Bild Stefan Matheis" />
-            </div>
-            <div class="column">
-              <p>Fragen? Anregungen? Presse. Sponsoring.</p>
-              <p class="mt-4">
-                <strong>Stefan Matheis</strong><br />
-                <a href="mailto:stefan.matheis@schachmatt-ulm.de"
-                  >✉️ stefan.matheis@schachmatt-ulm.de</a
-                ><br />
-                <a href="tel:004916097048114">📞 +49 160 970 48 114</a>
-              </p>
-
-              <p class="mt-5 is-size-6 has-text-grey-light">
-                Copyright/Urheberrecht aller Bilder:
-                <a
-                  href="https://www.kappelmeier-fotografie.de"
-                  class="has-text-grey-light"
-                  >Kappelmeier Fotografie</a
-                >
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="columns">
-        <div class="column competitions">
-          <div class="columns is-mobile">
-            <div class="column has-text-centered">
-              <a href="//munich-sunrise.gelungen.es">
-                <img
-                  src="/images/munich-sunrise.svg"
-                  alt="Logo Munich Sunrise"
-                />
-                <p class="has-text-centered">
-                  <span>Munich</span>
-                  <span>Sunrise</span>
-                  <span class="tag mt-2 is-info">Early Bird</span>
-                </p>
-              </a>
-            </div>
-            <div class="column has-text-centered">
-              <a href="//www.schachmatt-ulm.de">
-                <img
-                  src="/images/schachmatt-ulm.svg"
-                  alt="Logo Schachmatt Ulm"
-                />
-                <p class="has-text-centered">
-                  <span>Schachmatt</span>
-                  <span>Ulm</span>
-                  <span class="tag mt-2 is-success">So, 01.02.26</span>
-                </p>
-              </a>
-            </div>
-            <div class="column has-text-centered">
-              <a href="//muensterturmlauf-ulm.gelungen.es">
-                <img
-                  src="/images/muensterturmlauf-ulm.svg"
-                  alt="Logo Münsterturmlauf Ulm"
-                />
-                <p class="has-text-centered">
-                  <span>Münsterturmlauf</span>
-                  <span>Ulm</span>
-                  <span class="tag mt-2 is-success">Fr, 22.05.26</span>
-                </p>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="column competitions">
-          <div class="columns is-mobile">
-            <div class="column has-text-centered">
-              <a href="//www.fire-trail-nesselwang.de">
-                <img
-                  src="/images/fire-trail-nesselwang.svg"
-                  alt="Logo Fire Trail Nesselwang"
-                />
-                <p class="has-text-centered">
-                  <span>Fire Trail</span>
-                  <span>Nesselwang</span>
-                  <span class="tag mt-2">Oktober '26</span>
-                </p>
-              </a>
-            </div>
-            <div class="column has-text-centered">
-              <a href="//www.schanzenlauf-oberstdorf.de">
-                <img
-                  src="/images/schanzenlauf-oberstdorf.svg"
-                  alt="Logo Schanzenlauf Oberstdorf"
-                />
-                <p class="has-text-centered">
-                  <span>Schanzenlauf</span>
-                  <span>Oberstdorf</span>
-                </p>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
+  <CompetitionsFooter mail="stefan.matheis@schachmatt-ulm.de">
+    Copyright/Urheberrecht aller Bilder:
+    <a href="https://www.kappelmeier-fotografie.de" class="has-text-grey-light"
+      >Kappelmeier Fotografie</a
+    >
+  </CompetitionsFooter>
 </template>
 
 <style>
